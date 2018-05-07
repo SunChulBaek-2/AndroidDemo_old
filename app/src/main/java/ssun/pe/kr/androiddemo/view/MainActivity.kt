d1e7d9dc03be4d746a9e23c807bce8f192b5d7cf
x1210x <x1210x@gmail.com>
2018-05-07 11:29:08 +0900

    initial commit

---
+++ app/src/main/java/ssun/pe/kr/androiddemo/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package ssun.pe.kr.androiddemo.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ssun.pe.kr.androiddemo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
