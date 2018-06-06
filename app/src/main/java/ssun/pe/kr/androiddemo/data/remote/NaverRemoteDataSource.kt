35ee0de0d2fb259593bd0e55699289765f622f6b
백선철 <x1210x@gmail.com>
2018-06-06 12:56:36 +0900

    로그 추가

--- app/src/main/java/ssun/pe/kr/androiddemo/data/remote/NaverRemoteDataSource.kt
+++ app/src/main/java/ssun/pe/kr/androiddemo/data/remote/NaverRemoteDataSource.kt
@@ -1,5 +1,6 @@
 package ssun.pe.kr.androiddemo.data.remote
 
+import android.util.Log
 import kotlinx.coroutines.experimental.Deferred
 import ssun.pe.kr.androiddemo.data.NaverDataSource
 import ssun.pe.kr.androiddemo.data.model.Result
@@ -7,7 +8,15 @@ import ssun.pe.kr.androiddemo.network.NaverService
 import ssun.pe.kr.androiddemo.network.RetrofitCreator
 
 class NaverRemoteDataSource : NaverDataSource {
+
+    companion object {
+        private const val TAG = "NaverRemoteDataSource"
+    }
+
     private val mService: NaverService = RetrofitCreator.create().create(NaverService::class.java)
 
-    override fun searchBlog(query: String): Deferred<Result> = mService.searchShop(query)
+    override fun searchBlog(query: String): Deferred<Result> {
+        Log.d(TAG, "[x1210x]${Thread.currentThread()} searchBlog($query)")
+        return mService.searchShop(query)
+    }
 }
--- app/src/main/java/ssun/pe/kr/androiddemo/view/MainViewModel.kt
+++ app/src/main/java/ssun/pe/kr/androiddemo/view/MainViewModel.kt
@@ -4,12 +4,17 @@ import android.databinding.BaseObservable
 import android.databinding.ObservableArrayList
 import android.databinding.ObservableField
 import android.databinding.ObservableList
+import android.util.Log
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
 import ssun.pe.kr.androiddemo.data.NaverRepository
 import ssun.pe.kr.androiddemo.data.model.Item
 
 class MainViewModel : BaseObservable() {
+
+    companion object {
+        private const val TAG = "MainViewModel"
+    }
 
     val inProgress: ObservableField<Boolean> = ObservableField(false)
     val items: ObservableList<Item> = ObservableArrayList()
@@ -20,6 +25,7 @@ class MainViewModel : BaseObservable() {
         launch(UI) {
             try {
                 val result = NaverRepository.searchBlog(query).await()
+                Log.d(TAG, "[x1210x]${Thread.currentThread()} searchBlog($query)")
                 items.clear()
                 items.addAll(result.items)
             } catch (e: Exception) {
