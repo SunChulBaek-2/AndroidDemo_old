939468673dea5ce2dc35426fbb7289ce7f622521
백선철 <sunchulbaek@ncsoft.com>
2018-08-21 12:41:46 +0900

    KTX 적용

--- app/src/main/java/ssun/pe/kr/androiddemo/view/MainActivity.kt
+++ app/src/main/java/ssun/pe/kr/androiddemo/view/MainActivity.kt
@@ -2,6 +2,7 @@ package ssun.pe.kr.androiddemo.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.transaction
 import ssun.pe.kr.androiddemo.R
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_container)
 
-        MainFragment().let { fragment ->
-            supportFragmentManager.beginTransaction()
-                    .add(R.id.container, fragment, MainFragment.TAG)
-                    .commit()
+        supportFragmentManager.transaction {
+            add(R.id.container, MainFragment(), MainFragment.TAG)
         }
     }
 }
