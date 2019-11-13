d9300d9bcce2ed036c515b85a55a2bf383d7c68f
백선철 <sunchulbaek@ncsoft.com>
2019-11-13 14:39:28 +0900

    검색 시 키보드 내림

--- app/src/main/java/ssun/pe/kr/androiddemo/presentation/main/MainActivity.kt
+++ app/src/main/java/ssun/pe/kr/androiddemo/presentation/main/MainActivity.kt
@@ -1,6 +1,9 @@
 package ssun.pe.kr.androiddemo.presentation.main
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
@@ -22,27 +25,35 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main).apply {
-            lifecycleOwner = this@MainActivity
-            viewModel = this@MainActivity.viewModel
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+            .apply {
+                lifecycleOwner = this@MainActivity
+                viewModel = this@MainActivity.viewModel
 
-            toolbar.inflateMenu(R.menu.menu_main)
-            (toolbar.menu.findItem(R.id.search).actionView as SearchView).setOnQueryTextListener(
-                object : SearchView.OnQueryTextListener {
-                    override fun onQueryTextSubmit(query: String?): Boolean = query?.let {
-                        this@MainActivity.viewModel.search(query)
-                        true
-                    } ?: false
+                toolbar.inflateMenu(R.menu.menu_main)
+                (toolbar.menu.findItem(R.id.search).actionView as SearchView).setOnQueryTextListener(
+                    object : SearchView.OnQueryTextListener {
+                        override fun onQueryTextSubmit(query: String?): Boolean = query?.let {
+                            this@MainActivity.viewModel.search(query)
+                            hideKeyboard(binding.root.findFocus())
+                            true
+                        } ?: false
 
-                    override fun onQueryTextChange(newText: String?): Boolean = false
-                })
+                        override fun onQueryTextChange(newText: String?): Boolean = false
+                    })
 
-            rvItems.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
-            rvItems.adapter = MainAdapter(this@MainActivity.viewModel, this@MainActivity)
-            val deco = DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL)
-            deco.setDrawable(ContextCompat.getDrawable(this@MainActivity, R.drawable.main_deco)!!)
-            rvItems.addItemDecoration(deco)
-        }
+                rvItems.layoutManager =
+                    LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+                rvItems.adapter = MainAdapter(this@MainActivity.viewModel, this@MainActivity)
+                val deco = DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL)
+                deco.setDrawable(
+                    ContextCompat.getDrawable(
+                        this@MainActivity,
+                        R.drawable.main_deco
+                    )!!
+                )
+                rvItems.addItemDecoration(deco)
+            }
         setObservers()
     }
 
@@ -58,5 +69,10 @@ class MainActivity : BaseActivity() {
         } else {
             super.onBackPressed()
         }
+    }
+
+    private fun hideKeyboard(v: View) {
+        val imm = (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+        imm.hideSoftInputFromWindow(v.windowToken, 0)
     }
 }
