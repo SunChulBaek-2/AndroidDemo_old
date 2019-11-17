fdc19631e1217e819dfc0f79e0a904face5bb6bb
백선철 <sunchulbaek@ncsoft.com>
2019-11-18 07:55:05 +0900

    리팩토링

--- lib/src/main/java/ssun/pe/kr/androiddemo/presentation/NetworkState.kt
+++ lib/src/main/java/ssun/pe/kr/androiddemo/presentation/NetworkState.kt
@@ -27,8 +27,13 @@ data class NetworkState private constructor(
     val status: Status,
     val msg: String? = null) {
     companion object {
-        val LOADED = NetworkState(Status.SUCCESS)
-        val LOADING = NetworkState(Status.RUNNING)
-        fun error(msg: String?) = NetworkState(Status.FAILED, msg)
+        val LOADED =
+            NetworkState(Status.SUCCESS)
+        val LOADING =
+            NetworkState(Status.RUNNING)
+        fun error(msg: String?) = NetworkState(
+            Status.FAILED,
+            msg
+        )
     }
 }
