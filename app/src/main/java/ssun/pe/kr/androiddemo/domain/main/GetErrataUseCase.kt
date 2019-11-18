9be6cf9d4fcd1abaab86126d21d7d4b05651ee2a
백선철 <sunchulbaek@ncsoft.com>
2019-11-18 16:48:15 +0900

    소스정리

--- app/src/main/java/ssun/pe/kr/androiddemo/domain/main/GetErrataUseCase.kt
+++ app/src/main/java/ssun/pe/kr/androiddemo/domain/main/GetErrataUseCase.kt
@@ -10,25 +10,4 @@ class GetErrataUseCase : SingleUseCase<String, ErrataResult>() {
     private val repository: SearchRepository = DefaultSearchRepository()
 
     override fun execute(p: String): Single<ErrataResult> = repository.errata(p)
-//    {
-//        val result = MutableLiveData<ErrataResult>()
-//        val networkState = MutableLiveData<NetworkState>()
-//        scope.launch {
-//            networkState.postValue(NetworkState.LOADING)
-//            try {
-//                result.postValue(repository.errata(p))
-//                networkState.postValue(NetworkState.LOADED)
-//            } catch (e: HttpException) {
-//                networkState.postValue(NetworkState.error("error code: ${e.code()}"))
-//                e.printStackTrace()
-//            } catch (e: Exception) {
-//                networkState.postValue(NetworkState.error(e.message ?: "unknown error"))
-//                e.printStackTrace()
-//            }
-//        }
-//        return Once(
-//            result = result,
-//            networkState = networkState
-//        )
-//    }
 }
