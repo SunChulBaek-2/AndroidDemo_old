228f5c6216d63b22ae5cbdeae5eb45e6b7a031b4
백선철 <sunchulbaek@ncsoft.com>
2019-11-19 10:19:11 +0900

    소스정리

--- lib/src/main/java/ssun/pe/kr/androiddemo/domain/FlowableUseCase.kt
+++ lib/src/main/java/ssun/pe/kr/androiddemo/domain/FlowableUseCase.kt
@@ -2,7 +2,4 @@ package ssun.pe.kr.androiddemo.domain
 
 import io.reactivex.Flowable
 
-abstract class FlowableUseCase<in P, R> : UseCase<P, Flowable<R>>() {
-
-    abstract override operator fun invoke(p: P): Flowable<R>
-}
+abstract class FlowableUseCase<in P, R> : UseCase<P, Flowable<R>>()
--- lib/src/main/java/ssun/pe/kr/androiddemo/domain/SingleUseCase.kt
+++ lib/src/main/java/ssun/pe/kr/androiddemo/domain/SingleUseCase.kt
@@ -2,7 +2,4 @@ package ssun.pe.kr.androiddemo.domain
 
 import io.reactivex.Single
 
-abstract class SingleUseCase<in P, R> : UseCase<P, Single<R>>() {
-
-    abstract override operator fun invoke(p: P): Single<R>
-}
+abstract class SingleUseCase<in P, R> : UseCase<P, Single<R>>()
